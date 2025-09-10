5d94ff14edcbad1493ff1f1f824915451451a8fc
Youssifahmed12 <youssefbro12397@gmail.com>
2025-09-10 10:17:12 +0300

    refactroing

--- main.go
+++ main.go
@@ -6,22 +6,7 @@ import (
 	server "github.com/Youssifahmed12/lgwt-project/httpserver"
 )
 
-// in_memory_player_store.go
-func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
-}
 
-type InMemoryPlayerStore struct {
-	store map[string]int
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-	i.store[name]++
-}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return i.store[name]
-}
 
 func main() {
 	s := &server.PlayerServer{Store: NewInMemoryPlayerStore()}
